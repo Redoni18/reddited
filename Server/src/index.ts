bb7ab986f629ac3cb77edf12cea2f542a7e1a060
Redon Emini <redonemini18@gmail.com>
2023-10-01 23:59:52 +0200

    user sessions with pg

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { __prod__, __secret__ } from "./constants";
 import microConfig from "./mikro-orm.config"
 import express from "express"
 import { ApolloServer } from "@apollo/server"
@@ -11,6 +11,10 @@ import { json } from "body-parser";
 import { expressMiddleware } from "@apollo/server/express4";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from './resolvers/user';
+import session from "express-session"
+import connectPgSimple from "connect-pg-simple"
+import pg from "pg"
+
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -20,6 +24,34 @@ const main = async () => {
 
     const app = express();
     const cors = require('cors')
+
+    const pgSession = connectPgSimple(session);
+
+    const pgPool = new pg.Pool({
+        user: 'your-db-user',
+        host: 'your-db-host',
+        database: 'your-db-name',
+        password: 'your-db-password',
+        port: 5432,
+    });
+
+    app.use(session({
+        name: "qid",
+        store: new pgSession({
+            pool : pgPool,
+            disableTouch: true,
+            tableName : 'user_sessions'
+        }),
+        secret: __secret__,
+        resave: false,
+        cookie: { 
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+            httpOnly: true,
+            secure: __prod__, //cookie only works in https,
+            sameSite: 'lax'
+        } // 30 days
+        // Insert express-session options here
+    }));
 
 
     const schema:GraphQLSchema = await buildSchema({
